23921ad312f6329d7e544f4c9b53fb3faa971662
AMAN DEEP SINGH <as0300@srmist.edu.in>
2025-03-14 13:19:50 +0530

    Add a new program to print a square pattern of asterisks

---
+++ Basics/Patterns/P1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void nforrest(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+         cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int num;
+    cin>>num;
+    nforrest(num);
+    return 0;
+
+}
