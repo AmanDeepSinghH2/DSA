164575433ed70a6c3ba510b499b9d1312ea7affa
AMAN DEEP SINGH <as0300@srmist.edu.in>
2025-03-14 13:38:21 +0530

    Add Visual Studio Code configuration and new pattern programs

---
+++ Basics/Patterns/p4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void nforrest(int n){
+    for(int i=0;i<n;i++){
+    for(int j=0;j<=i;j++){
+        cout<<i+1;
+    }
+    cout<<endl; 
+    }
+}
+
+int main(){
+    int num;
+    cin>>num;
+    nforrest(num);
+    return 0;
+}
