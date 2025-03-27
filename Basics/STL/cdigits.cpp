1c24342d2bfd571953f0c1727553b6a7c070c49c
AMAN DEEP SINGH <as0300@srmist.edu.in>
2025-03-27 16:30:11 +0530

    Add programs to count digits, reverse an array, and implement selection sort

---
+++ Basics/STL/cdigits.cpp
@@ -0,0 +1,15 @@
+#include <bits-stdc++.h>
+using namespace std;
+
+int cdigits(int n) {
+    if (n == 0) return 1;
+    return log10(abs(n)) + 1;
+}
+
+int main(){
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    cout << "The number of digits in " << number << " is " << cdigits(number) << endl;
+    return 0;
+}
