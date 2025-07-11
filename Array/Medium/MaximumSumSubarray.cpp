90ad96dd471d58e3370648e5068b31851e4777b7
Aman Deep Singh <as0300@srmist.edu.in>
2025-07-11 21:14:40 +0530

    Add implementation for maximum sum subarray problem

---
+++ Array/Medium/MaximumSumSubarray.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubArray(vector<int> &nums){
+    int n = nums.size();
+    int maxi = INT_MIN;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += nums[i];
+        if (sum > maxi) {
+            maxi = sum;
+        }
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+    return maxi;
+}
+
+int main() {
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int result = maxSubArray(nums);
+    cout << "Maximum subarray sum: " << result << endl;
+    return 0;
+}
