9b7b6aa769392660c3b4034e367af858fcde38ba
Aman Deep Singh <as0300@srmist.edu.in>
2025-12-09 21:06:29 +0530

    Added SetMatrixZero solution

---
+++ Blind 75/SetMatrixZeros.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setZeros(vector<vector<int>>&matrix){
+    int m=matrix.size();
+    int n=matrix[0].size();
+    vector<int>row (m);
+    vector<int> col (n);
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(matrix[i][j]==0){
+                
+            }
+        }
+        
+    }
+}
