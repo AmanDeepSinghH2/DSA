c3f9005eab3d8e6d3b39d79667cf4a09fb729fb0
Aman Deep Singh <as0300@srmist.edu.in>
2026-02-01 00:20:57 +0530

    Modified Solution

--- Linked List/DeleteTheMiddleNode.cpp
+++ Linked List/DeleteTheMiddleNode.cpp
@@ -8,18 +8,27 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        ListNode* slow=head;
-        ListNode* fast=head->next->next;
-        while(fast && fast->next){
-            slow=slow->next;
-            fast=fast->next->next;
+        if (head == nullptr || head->next == nullptr) {
+            delete head;
+            return nullptr;
         }
-        ListNode* middle=slow->next;
-        slow->next=slow->next->next;
+
+        ListNode* slow = head;
+        ListNode* fast = head->next->next;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* middle = slow->next;
+        slow->next = slow->next->next;
         delete middle;
+
         return head;
     }
 };
