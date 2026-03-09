81293ecdfe473697d6293a255a886f6dff6b2110
Aman Deep Singh <as0300@srmist.edu.in>
2026-03-09 22:43:05 +0530

    added solution to finding the longest common prefix

---
+++ String/Easy and Medium/LongestCommonPrefix.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        string ans="";
+        string f=strs[0];
+        string l=strs[strs.size()-1];
+        int len=min(f.size(),l.size());
+        for(int i=0;i<=len;i++){
+            if(f[i]!=l[i]) break;
+            ans+=f[i];
+        }
+        return ans;
+    }
+};
