a6b922206ed0880c3ff0b99139955f5ad43905cd
Aman Deep Singh <as0300@srmist.edu.in>
2026-03-09 23:25:07 +0530

    added solution for Rotate String

---
+++ String/Easy and Medium/RotateString.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    bool rotateString(string s, string goal){
+        if(s.size()!=goal.size()) return false;
+        string ds=s+s;
+        return ds.find(goal)!=string::npos;
+    }
+};
