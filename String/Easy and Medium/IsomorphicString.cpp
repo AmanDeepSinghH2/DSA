b619c6632b27c33dfb1d88ad0481365bfa026472
Aman Deep Singh <as0300@srmist.edu.in>
2026-03-09 23:11:28 +0530

    added solution to Isomorphic String

---
+++ String/Easy and Medium/IsomorphicString.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char> st;
+        unordered_map<char,char> ts;
+
+        for(int i=0;i<s.size();i++){
+            char c1=s[i];
+            char c2=t[i];
+
+            if(st.count(c1)){
+                if(st[c1]!=c2) return false;
+            }
+            else{
+                st[c1]=c2;
+            }
+
+            if(ts.count(c2)){
+                if(ts[c2]!=c1) return false;
+            }
+            else{
+                ts[c2]=c1;
+            }
+        }
+        return true;
+    }
+};
