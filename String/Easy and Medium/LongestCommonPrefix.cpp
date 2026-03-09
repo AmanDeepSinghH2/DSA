262316d57784fedfdb4602ffa9c4a20722622a3b
Aman Deep Singh <as0300@srmist.edu.in>
2026-03-09 22:48:28 +0530

    modified solution

--- String/Easy and Medium/LongestCommonPrefix.cpp
+++ String/Easy and Medium/LongestCommonPrefix.cpp
@@ -4,12 +4,13 @@ using namespace std;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
         sort(strs.begin(),strs.end());
         string ans="";
         string f=strs[0];
         string l=strs[strs.size()-1];
         int len=min(f.size(),l.size());
-        for(int i=0;i<=len;i++){
+        for(int i=0;i<len;i++){
             if(f[i]!=l[i]) break;
             ans+=f[i];
         }
