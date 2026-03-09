d3db3d6704011fa6b7f638f1cb4981165e2c101c
Aman Deep Singh <as0300@srmist.edu.in>
2026-03-09 23:32:18 +0530

    added solution for valid anagram

---
+++ String/Easy and Medium/ValidAnagram.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for (int i = 0; i < s.size(); i++){
+            if (s[i] != t[i]){
+                return false;
+            } return s ==t;
+        } return 0;
+    }
+};
