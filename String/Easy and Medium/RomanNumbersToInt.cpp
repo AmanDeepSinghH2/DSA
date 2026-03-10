17a57ca1cb368ac09f9af557c47a5d5b0e153e2e
Aman Deep Singh <as0300@srmist.edu.in>
2026-03-10 14:27:34 +0530

    added solution to Roman Numbers to integer

---
+++ String/Easy and Medium/RomanNumbersToInt.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char,int> mpp={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        int res=0;
+        int n=s.size();
+        for(int i=0;i<n-1;i++){
+            if(mpp[s[i]]<mpp[s[i+1]]) res-=mpp[s[i]];
+            else res+=mpp[s[i]];
+        }
+        return res+mpp[s.back()];
+    }
+};
