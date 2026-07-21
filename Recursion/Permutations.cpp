1341f894760303ca26d6ee90a296215be1039679
AmanDeepSinghH2 <amandeepsingh090206@gmail.com>
2026-07-22 02:44:05 +0530

    added solution to LC Permutations

---
+++ Recursion/Permutations.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        findperm(0,nums,ans);
+        return ans;
+    }
+    void findperm(int ind,vector<int>&nums,vector<vector<int>>&ans){
+        if(ind==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            findperm(ind+1,nums,ans);
+            swap(nums[ind],nums[i]);
+        }
+    }
+};
